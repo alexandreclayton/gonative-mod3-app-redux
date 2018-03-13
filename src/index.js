29b5d86f51397e241f30334c382ddcbb9ea1a73e
alexandresette <alexandreclayton@gmail.com>
2018-03-13 20:52:57 -0300

    [Configurando novo App]
    - Ajustando o projeto para uma nova abordagem

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
 import React from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from 'store';
 import 'config/ReactotronConfig';
-import TodoList from 'pages/TodoList';
 
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <View />
   </Provider>
 );
 
