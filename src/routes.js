d653124af972338bb229dc2fc4164d52a1adb332
alexandresette <alexandreclayton@gmail.com>
2018-03-21 10:23:35 -0300

    [Estilizando o header]
    - instalando "yarn add color"
    - adicionando navigationOptions para estilizar a header

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
 import { StackNavigator } from 'react-navigation';
+import { colors } from 'styles';
 import Main from 'pages/main';
 import Favorites from 'pages/favorites';
 
 export default StackNavigator({
   Main: { screen: Main },
   Favorites: { screen: Favorites },
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: colors.primaryDark,
+    },
+    headerTintColor: colors.white,
+    headerBackTitle: null,
+  },
 });
