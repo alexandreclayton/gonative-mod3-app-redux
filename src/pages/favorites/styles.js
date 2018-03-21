e532fcaf9e2300e1505658a934f0a1408d9aa793
alexandresette <alexandreclayton@gmail.com>
2018-03-21 11:03:02 -0300

    [Favoritos - Ajustes]
    - Ajustando importacoes
    - Ajustanto styles

--- src/pages/favorites/styles.js
+++ src/pages/favorites/styles.js
@@ -1,10 +1,16 @@
 import { StyleSheet } from 'react-native';
-import { colors } from 'styles';
+import { colors, matrics } from 'styles';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.primary,
+  },
+  empty: {
+    alignSelf: 'center',
+    color: colors.white,
+    marginTop: matrics.baseMargin * 2,
+    fontSize: 14,
   },
 });
 
